eebe968d9a58a4704d645a7239818a63e0704c9b
marcussousa <marcus.sousa@bemobi.com.br>
2019-03-14 22:11:00 -0300

    first commit

---
+++ src/handlers.py
@@ -0,0 +1,27 @@
+from aiohttp import web, ClientSession
+routes = web.RouteTableDef()
+
+
+@routes.get('/cep/{cep}')
+async def find_by_cep(request):
+    client = ClientSession()
+    cep = request.match_info.get('cep')
+    async with client as session:
+        async with session.get('https://viacep.com.br/ws/%s/json' % cep) as resp:
+            print(resp.status)
+            text = await resp.text()
+    return web.Response(text=text, content_type='application/json')
+
+
+@routes.get('/address/{uf}/{city}/street')
+async def find_by_cep(request):
+    client = ClientSession()
+    code = request.match_info.get('uf')
+    city = request.match_info.get('city')
+    street = request.match_info.get('street')
+
+    async with client as session:
+        async with session.get('https://viacep.com.br/ws/%s/%s/%s/json' % (code, city, street)) as resp:
+            print(resp.status)
+            text = await resp.text()
+    return web.Response(text=text, content_type='application/json')
---
+++ src/http_server.py
@@ -0,0 +1,6 @@
+from aiohttp import web
+from src import handlers
+
+app = web.Application()
+app.add_routes(handlers.routes)
+web.run_app(app)
